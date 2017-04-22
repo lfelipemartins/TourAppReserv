ac450d0332e5916fa1f2021bf59b01d294725396
Felipe Martins <lfelipemartins.88@gmail.com>
2017-04-22 18:58:26 -0300

    Tela de login e tela de detalhes, sem back.

--- app/src/main/java/felipemartins/com/br/tourappreserv/LoginActivity.java
+++ app/src/main/java/felipemartins/com/br/tourappreserv/LoginActivity.java
@@ -1,18 +1,27 @@
 package felipemartins.com.br.tourappreserv;
 
+
+
 import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+
+
+public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        // Set up the login form.
+
+
+
     }
 
 
 
-
-
 }
+
