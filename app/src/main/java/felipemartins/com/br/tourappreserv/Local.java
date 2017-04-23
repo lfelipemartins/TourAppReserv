c5a067b16c65451225f2586b2a8ecdec5e1fed85
Felipe Martins <lfelipemartins.88@gmail.com>
2017-04-23 15:32:30 -0300

    telas extras e recicleview - revisado

--- app/src/main/java/felipemartins/com/br/tourappreserv/Local.java
+++ app/src/main/java/felipemartins/com/br/tourappreserv/Local.java
@@ -8,7 +8,34 @@ public class Local {
 
     private String nome;
 
+    private String descricaoCurta, descricaolonga;
+
+    private String local;
+
     public Local(){}
+
+    public String getDescricaoCurta() {
+        return descricaoCurta;
+    }
+
+    public void setDescricaoCurta(String descricaoCurta) {
+        this.descricaoCurta = descricaoCurta;
+    }
+
+    public String getDescricaolonga() {
+        return descricaolonga;
+    }
+
+    public void setDescricaolonga(String descricaolonga) {
+        this.descricaolonga = descricaolonga;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+    public void setLocal(String local) {
+        this.local = local;
+    }
 
     public String getNome() {
         return nome;
